eb476b2be0bf57c461011ba772a0df65cd1abb97
Adrian Lebmeier <adrian.lebmeier@tha.de>
2024-03-06 06:14:40 +0100

    new test

---
+++ client/__test__/controller/registryHandler.test.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { registerClimber, registerHallowner } from '../controller/'
+
+describe("Register Functions", () => {
+  beforeEach(() => {
+    axios.post = jest.fn().mockResolvedValue({ status: 200, data: "Success" });
+  });
+
+  // Test Case 1: registerClimber
+  it("should register climber successfully", async () => {
+    const query = "some_query";
+    const params = "some_params";
+    const response = await registerClimber(query, params);
+    expect(axios.post).toHaveBeenCalledWith(
+      "http://35.193.105.8:3000/register_climber",
+      { query: query, params: params }
+    );
+    expect(response.status).toBe(200);
+    expect(response.data).toBe("Success");
+  });
+
+  // Test Case 2: registerHallowner
+  it("should register hall owner successfully", async () => {
+    const query = "some_query";
+    const params = "some_params";
+    const response = await registerHallowner(query, params);
+    expect(axios.post).toHaveBeenCalledWith(
+      "http://35.193.105.8:3000/register_hallowner",
+      { query: query, params: params }
+    );
+    expect(response.status).toBe(200);
+    expect(response.data).toBe("Success");
+  });
+});
